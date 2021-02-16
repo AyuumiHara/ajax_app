dff16b582256a70ab459c12ce8b504c33cbe7d90
AyuumiHara <ayuumi027289@gmail.com>
2021-02-16 11:31:07 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'#no4208の新規投稿実装用
+  root to: 'posts#index'
   post 'posts', to: 'posts#create'#no4209の投稿作業実装用
  end
